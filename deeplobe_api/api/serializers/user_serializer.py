049504123cb92f4f12af746fdec68e700d1d7cee
raviteja-soulpage <raviteja.marum@soulpageit.com>
2021-04-14 09:54:37 +0530

    "Added usename in serializers"

--- deeplobe_api/api/serializers/user_serializer.py
+++ deeplobe_api/api/serializers/user_serializer.py
@@ -7,6 +7,7 @@ class UserSerializer(serializers.ModelSerializer):
         model = User
         fields = [
             "id",
+            "username"
             "email",
             "first_name",
             "last_name",
